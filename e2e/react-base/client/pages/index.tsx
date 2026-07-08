d831b5697435ac6ade078fbf27865ad242737fff
teneburu <teneburu@proton.me>
2026-07-08 18:45:04 +0200

    feat: create react-base e2e fixture

---
+++ e2e/react-base/client/pages/index.tsx
@@ -0,0 +1,11 @@
+export function getMeta() {
+  return { title: 'React base e2e' }
+}
+
+export function getData() {
+  return { message: 'Hello from getData' }
+}
+
+export default function Index() {
+  return <p>React base e2e</p>
+}
---
+++ e2e/react-base/client/pages/users/[id].tsx
@@ -0,0 +1,3 @@
+export default function User({ params }: { params: Record<string, string> }) {
+  return <p>User: {params.id}</p>
+}
