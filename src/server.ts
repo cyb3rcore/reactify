22a11a6646c31c0c4b301ddf08cf7d9367a16b9e
teneburu <teneburu@proton.me>
2026-07-16 13:42:21 +0200

    refactor: move server.ts to server/index.ts barrel

--- src/server.ts
+++
@@ -1,4 +0,0 @@
-export { prepareServer, createRoutes } from './react/server.js'
-export { getContext, getReq, getReply, getServer } from './react/rsc-context.js'
-
-export { redirect, isRedirectError } from './react/redirect.js'
